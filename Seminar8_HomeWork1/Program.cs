1792ec9434560403d47487de443234428db91069
Aleks37 <lao35180972643335589007>
2023-08-16 21:47:15 +0400

    Add Changes in Seminar8_HomeWork1

--- Seminar8_HomeWork1/Program.cs
+++ Seminar8_HomeWork1/Program.cs
@@ -1,8 +1,20 @@
 ﻿// Напишите программу, кот на вход принимает 3-значное число и на выходе показывает 2ю цифру этого числа.
 Console.Write ("Введите трёхзначное число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int SecondNumber = n / 10;
-SecondNumber = SecondNumber % 10;
+if (n/100 == 0 || n/1000 != 0)
 {
-    Console.Write ($"Вторая цифра: {SecondNumber}");
+    Console.Write ("Данное число не является трёхзначным!");
+}
+else
+{
+    Console.Write ("Число действительно трёхзначное. ");
+    int SecondNumber = n / 10;
+    SecondNumber = SecondNumber % 10;
+    if (SecondNumber > 0)
+        {Console.Write ($"Вторая цифра данного числа: {SecondNumber}");}
+        else
+        {
+            SecondNumber = SecondNumber * (-1);
+            Console.Write ($"Вторая цифра данного числа: {SecondNumber}");
+        }
 }
