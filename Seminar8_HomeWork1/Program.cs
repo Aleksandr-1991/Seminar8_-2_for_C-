28746e1bac9d87e41e165e7af7ff567451a0bf9f
Aleks37 <lao35180972643335589007>
2023-11-27 21:36:03 +0400

    Add changes from 27.11.23

--- Seminar8_HomeWork1/Program.cs
+++ Seminar8_HomeWork1/Program.cs
@@ -1,4 +1,4 @@
-﻿// Напишите программу, кот на вход принимает 3-значное число и на выходе показывает 2ю цифру этого числа.
+﻿// Задача 10. Нап программу, кот приним 3-значное число и на выходе показывает 2ю цифру этого числа.
 Console.Write ("Введите трёхзначное число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 if (n/100 == 0 || n/1000 != 0)
@@ -8,13 +8,7 @@ if (n/100 == 0 || n/1000 != 0)
 else
 {
     Console.Write ("Число действительно трёхзначное. ");
-    int SecondDigit = n / 10;
-    SecondDigit = SecondDigit % 10;
-    if (SecondDigit > 0)
-        {Console.Write ($"Вторая цифра данного числа: {SecondDigit}");}
-        else
-        {
-            SecondDigit = SecondDigit * (-1);
-            Console.Write ($"Вторая цифра данного числа: {SecondDigit}");
-        }
+    int SecondDigit = n / 10 % 10;
+    SecondDigit = (SecondDigit >= 0) ? SecondDigit : -SecondDigit;
+    Console.Write ($"Вторая цифра данного числа: {SecondDigit}");
 }
--- Seminar8_HomeWork2/Program.cs
+++ Seminar8_HomeWork2/Program.cs
@@ -1,25 +1,21 @@
-﻿// Напишите программу, кот выводит 3-юю цифру заданного числа 
+﻿// Зад 13. Нап программу, кот выводит 3-юю цифру заданного числа 
 // или сообщает, что 3ей цифры нет.
 Console.Write ("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-if (n/100 == 0)
-{
-    Console.Write ("Третьей цифры нет.");
-}
+if (n/100 == 0)  Console.Write ("Третьей цифры нет.");
 else
 {
-    while (n > 999 || n < (-999))               // if (n/1000 != 0)
+    while (n/1000 != 0)           // if (n > 999 || n < -999)
         {
         n = n/10;
         }
     int digit3 = n % 10;
-    if (digit3 > 0)
-    {
-        Console.Write ($"Третяя цифра данного числа: {digit3}");
-    }     
-    else
-    {
-        digit3 = digit3 * (-1);
-        Console.Write ($"Третяя цифра данного числа: {digit3}");
-    }
+    digit3 = (digit3 >= 0) ? digit3 : -digit3;
+    Console.Write ($"Третяя цифра данного числа: {digit3}");
+    // if (digit3 > 0)  Console.Write ($"Третяя цифра данного числа: {digit3}");     
+    // else
+    // {
+    //     digit3 = digit3 * (-1);
+    //     Console.Write ($"Третяя цифра данного числа: {digit3}");
+    // }
 }
